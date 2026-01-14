b4d0782cb3d71417d2fc16bdfa8a8aeea6c612a1
Adrian Moravec <amoravec@ablenetinc.com>
2026-01-14 17:12:06 -0600

    testing

--- 6923349.js
+++ 6923349.js
@@ -2,13 +2,14 @@
 
   window.addEventListener("load", function () {
     console.log("JAVASCRIPT ATTACHED 002");
-  });
 
-  const updateButton = loader
+    const updateButton = loader
       .getEngine()
       .getDocument()
       .querySelector('[data-role="update"]');
 
     console.log("update button: ", updateButton);
+
+  });
 
 })();
