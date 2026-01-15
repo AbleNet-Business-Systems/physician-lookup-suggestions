a4f61a75427ff1fd05fe0c61897e82d24f6d3360
Adrian Moravec <amoravec@ablenetinc.com>
2026-01-14 18:08:24 -0600

    testing

--- 6923349.js
+++ 6923349.js
@@ -1,15 +1,14 @@
 (function () {
   window.addEventListener("load", function () {
     loadAfterTimeout();
-    console.log("JAVASCRIPT ATTACHED 004");
+    console.log("JAVASCRIPT ATTACHED 005");
 
     // const updateButton = loader
     //   .getEngine()
     //   .getDocument();
     //   .querySelector('[data-role="update"]');
 
-    const updateButton = document.querySelector('[data-role="update"]');
-    console.log("update button: ", updateButton);
+    // const updateButton = document.querySelector('[data-role="update"]');
 
   });
 
@@ -17,8 +16,13 @@
     setTimeout(function() {
         console.log("timeout complete");
         const updateButton = document.querySelector('[data-role="update"]');
-        console.log("update button: ", updateButton);
+        updateButton.addEventListener("click", handleUpdate);
     }, 10000);
+  }
+
+  function handleUpdate() {
+    const url = window.location.href;
+    console.log("the url: ", url);
   }
 
 })();
