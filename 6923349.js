402a978ffb32d66fcb751d5e422f2c1009c72271
Adrian Moravec <amoravec@ablenetinc.com>
2026-01-15 10:02:13 -0600

    testing

--- 6923349.js
+++ 6923349.js
@@ -3,7 +3,7 @@
 
   window.addEventListener("load", function () {
     loadAfterTimeout();
-    console.log("JAVASCRIPT ATTACHED 006");
+    console.log("JAVASCRIPT ATTACHED 007");
 
     // const updateButton = loader
     //   .getEngine()
@@ -19,15 +19,25 @@
         console.log("timeout complete");
         const updateButton = document.querySelector('[data-role="update"]');
         updateButton.addEventListener("click", handleUpdate);
-    }, 8000);
+    }, 10000);
   }
 
   function handleUpdate() {
-    const url = window.location.href;
-    console.log("the url: ", url);
+    // const url = window.location.href;
+    // console.log("the url: ", url);
 
-    const fn = loader.getEngine().getDocument().getElementById(fnInputId);
+    const fn = loader.getEngine().getDocument().getElementById(fnInputId).input.value;
     console.log("fn: ", fn);
+
+    fetch("https://hooks.zapier.com/hooks/catch/24234008/ugkuyvf/", {
+      method: "POST",
+      body: JSON.stringify({
+        fn: fn
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
 
   }
 
