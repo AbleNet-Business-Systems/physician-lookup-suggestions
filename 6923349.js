0042231a381c11c4d1aa5371aca41fc8fb18b715
Adrian Moravec <amoravec@ablenetinc.com>
2026-01-16 14:22:13 -0600

    testing

--- 6923349.js
+++ 6923349.js
@@ -3,7 +3,7 @@
 
   window.addEventListener("load", function () {
     loadAfterTimeout();
-    console.log("JAVASCRIPT ATTACHED 011");
+    console.log("JAVASCRIPT ATTACHED 012");
   });
 
   function loadAfterTimeout() {
@@ -19,7 +19,7 @@
     console.log("LOADED");
     const fn = loader.getEngine().getDocument().getElementById(fnInputId).input.value;
 
-    fetch("https://pgdy4cgem3.execute-api.us-east-1.amazonaws.com/test/notify", {
+    fetch("https://pgdy4cgem3.execute-api.us-east-1.amazonaws.com/npi-query/notify", {
       method: "POST",
       body: JSON.stringify({
         fn: fn
