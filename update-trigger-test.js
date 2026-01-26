01986cdc74b9ddf3e7b5daf7e1fac02c77fa610b
Adrian Moravec <amoravec@ablenetinc.com>
2026-01-26 14:53:07 -0600

    testing version

---
+++ update-trigger-test.js
@@ -0,0 +1,58 @@
+(function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      const updateButton = document.querySelector('[data-role="update"]');
+      if (updateButton) {
+        updateButton.addEventListener("click", handleUpdate);
+      }
+    }, 15000);
+  });
+
+  function handleUpdate() {
+    const fnInputIds = [
+      120160623, 118780304, 118823933, 118824262, 118451600, 118616072,
+      118626705, 118445458, 118631436, 119335174, 118716311, 118699257,
+      118679395, 119020131, 118715807, 118813830, 118806546, 118836274,
+      118653861, 118643415, 118666370, 118753396, 119483997, 118603080,
+      118629078, 118595808, 118705065, 118616950, 118693049, 118658459,
+      118804348, 118497202, 118258144, 117601937, 107316407
+    ];
+
+    const formNumberInputIds = [
+        120160957, 118258148
+    ];
+
+    const currentFnInputId = fnInputIds.find((inputId) =>
+      loader.getEngine().getDocument().getElementById(inputId)
+    );
+
+    const currentFormNumberInputId = formNumberInputIds.find((id) =>
+        loader.getEngine().getDocument().getElementById(id)
+    );
+
+    const fnId = loader.getEngine().getDocument().getElementById(currentFnInputId).input.value;
+    const formNumber = loader.getEngine().getDocument().getElementById(currentFormNumberInputId).input.value;
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlFormNumber = urlParams.get("s");
+
+    console.log("json to send: ", {
+        fn: fnId ? fnId : "",
+        formNumber: formNumber ? formNumber : "",
+        urlFormNumber: urlFormNumber ? urlFormNumber : ""
+    });
+
+    // fetch("https://pgdy4cgem3.execute-api.us-east-1.amazonaws.com/npi-query/notify", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     fn: fnId ? fnId : "",
+    //     formNumber: formNumber ? formNumber : "",
+    //     urlFormNumber: urlFormNumber ? urlFormNumber : ""
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8"
+    //   }
+    // });
+  }
+
+})();
